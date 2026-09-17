bfd8940839848e75f270370076059cd9af133797
Katherine Andrade <katherine04andrade@gmail.com>
2026-09-16 22:50:44 -0300

    creación formularios cliente y cuenta

---
+++ alke_wallet/gestion/forms.py
@@ -0,0 +1,79 @@
+from django import forms
+from .models import *
+##### login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+## Formulario cliente
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['rut', 'nombre', 'apellido','email', 'direccion', 'telefono']
+        widgets = {
+            'rut':forms.TextInput(
+                attrs={'class':'form-control'}
+                ),
+            'nombre': forms.TextInput(
+                attrs={'class':'form-control'}
+            ),
+            'apellido': forms.TextInput(
+                attrs={'class':'form-control',}
+            ),
+            'email': forms.DateInput(
+                attrs={'class':'form-control'}
+            ),
+            'direccion': forms.DateInput(
+                attrs={'class':'form-control'}
+            ),
+            'telefono': forms.DateInput(
+                attrs={'class':'form-control'}
+            ),
+        }
+        labels = {
+            'rut': 'RUT:',
+            'nombre': 'Nombre:',
+            'apellido': 'Apellido:',
+            'email': 'Email:',
+            'direccion': 'Dirección:',
+            'telefono': 'Teléfono:',
+        }
+        
+        def clean_rut(self):
+            rut = self.cleaned_data['rut']
+            if len(rut) < 9:
+                raise forms.ValidationError("Debe ingresar un rut válido")
+            return rut
+
+### Formulario cuenta 
+class CuentaForm(forms.ModelForm):
+    class Meta:
+        model = Cuenta
+        fields = ['cliente', 'numero_cuenta', 'tipo_cuenta','saldo']
+        widgets = {
+            'cliente':forms.TextInput(
+                attrs={'class':'form-control'}
+                ),
+            'numero_cuenta': forms.TextInput(
+                attrs={'class':'form-control'}
+            ),
+            'tipo_cuenta': forms.TextInput(
+                attrs={'class':'form-control',}
+            ),
+            'saldo': forms.DecimalField(
+                attrs={'class':'form-control'}
+            ),
+        }
+        labels = {
+            'cliente': 'Cliente:',
+            'numero_cuenta': 'Numero de cuenta:',
+            'tipo_cuenta': 'Tipo de cuenta:',
+            'saldo': 'Saldo:',
+        }
+        
+        def clean_cliente(self):
+            cliente = self.cleaned_data['cliente']
+            if len(cliente) <=0:
+                raise forms.ValidationError("El campo cliente no puede estar vacío")
+            return cliente
+
+    
